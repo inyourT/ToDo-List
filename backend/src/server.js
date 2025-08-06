f1177af603274c6915eae2a093d085702cdfc360
inyourT <gagunatulis@gmail.com>
2025-08-06 19:35:26 +0700

    prepared for the deploment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
 
 import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
@@ -12,14 +15,20 @@ dotenv.config();
 console.log(process.env.MONGO_URI);
 
 const app = express();
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5001;
+
+// const __dirname = path.resolve;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 
 // midleware
-app.use(cors({
-  origin:"http://localhost:5173",
-    })
-  );
+if(process.env.NODE_ENV !== "production") {
+  app.use(cors({
+    origin:"http://localhost:5173",
+      })
+    );
+}
 app.use(express.json()); // this middelware wll parse json bodies; req.body
 app.use(rateLimiter);
 
@@ -30,6 +39,14 @@ app.use(rateLimiter);
 // })
 
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname,"../../frontend/dist")))
+
+  app.get("*",(req,res) =>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
